8b219c9fddc6bc088addd7957509e84f06990e08
Mateusz Brzezanski <mateusz.brzezanski@gmail.com>
2020-05-20 14:22:08 +0200

    feature added

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,8 +1,10 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
 
+
+  RATING_OPTIONS = (0..5).to_a.freeze
   validates :content, presence: true
   validates :restaurant_id, presence: true
   validates :rating, numericality: { only_integer: true }
-  validates :rating, inclusion: { in: (0..5).to_a}
+  validates :rating, inclusion: { in: RATING_OPTIONS }
 end
